61e7efb35c44380861915d0208b5fe3dfabc3739
Salvage <salvageblaine@gmail.com>
2023-12-13 18:30:16 -0800

    updated

--- main.c
+++ main.c
@@ -1,7 +1,14 @@
 #include "main.h"
 #include <stdio.h>
 #include <unistd.h>
-
+/**
+ * interactive_mode - this is the main function.
+ * file_mode - this is the main function
+ * @argc: a parameter that takes arguments.
+ * @argv: a parameter that takes arguments.
+ * @envp: a parameter that takes variables.
+ * Return: interactive_mode or file_mode.
+ */
 #define BLANK " "
 
 int interactive_mode(void);
@@ -12,5 +19,5 @@ int main(int argc, char *argv[], char *envp[])
 	if (isatty(STDIN_FILENO))
 		return (interactive_mode);
 	else
-		return file_mode(argc, argv, envp);
+		return (file_mode(argc, argv, envp));
 }
