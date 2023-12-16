7d35edec170108e69d553be8d0f81efc1dcad9a4
tdo-olowu <olowu.otayo@gmail.com>
2023-12-16 10:52:00 -0800

    updated

--- .old_shell/old_filemdoe.c
+++ .old_shell/old_filemdoe.c
@@ -12,42 +12,30 @@ void file_mode(int ac, char **av, char **env)
 {
 	size_t cmdlen = 0;
 	ssize_t bytes_read = -1;
-	char *cmdline = NULL, **argv = NULL, **envp = envcopy(env);
-	int eval = 1, exit_stat = 0;
+	char *cmdline = NULL, **argv = NULL, **envp = env;
+	int eval = 1;
 
-	(void)ac;
-	(void)av;
 	do {
 		bytes_read = getcmd(&cmdline, &cmdlen, stdin);
 		if (bytes_read < 0)
 		{
 			if (feof(stdin))
 			{
-				exit_stat = 0;
-				break;
+				free(cmdline);
+				exit(EXIT_SUCCESS);
 			}
+			else
+				perror("Couldn't read input for some reason. Try again.");
 		}
+		else if (bytes_read == 0)
+			eval = -1;
 		else
 		{
 			argv = make_tokens(cmdline, " ");
-			if (argv == NULL)
-				continue;
-			exit_stat = is_exit(argv);
-			if (exit_stat >= 0)
-			{
-				free_table(argv);
-				break;
-			}
-			if (exit_stat == -10)
-			{
-				free_table(argv);
-				eval = 1;
-				continue;
-			}
 			eval = evaluate(argv, envp);
 			free_table(argv);
 		}
+		/* /free(cmdline);*/
 	} while (eval == 1);
-	cleanup(cmdline, envp);
-	exit(exit_stat);
+	free(cmdline);
 }
